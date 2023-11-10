410dfb34657639ba3d66e578d8d2a70121b0dd0d
hildelio <hildelio@gmail.com>
2023-11-10 19:41:01 -0300

    Android - Projeto Login Social :iphone: :closed_lock_with_key: Req 07

--- app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
+++ app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.betrybe.sociallogin
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doOnTextChanged
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
 
+    private val inputEmail: TextInputLayout by lazy { findViewById(R.id.email_text_input_layout) }
+    private val inputPassword: TextInputLayout by lazy {
+        findViewById(R.id.password_text_input_layout)
+    }
+    private val loginButton: Button by lazy { findViewById(R.id.login_button) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        inputEmail.editText?.doOnTextChanged { _, _, _, _ -> validateButton() }
+        inputPassword.editText?.doOnTextChanged { _, _, _, _ -> validateButton() }
+    }
+    private fun validateButton() {
+        val isEmailNotEmpty = inputEmail.editText?.text?.isNotBlank() == true
+        val isPasswordNotEmpty = inputPassword.editText?.text?.isNotBlank() == true
+
+        loginButton.isEnabled = isEmailNotEmpty && isPasswordNotEmpty
     }
 }
