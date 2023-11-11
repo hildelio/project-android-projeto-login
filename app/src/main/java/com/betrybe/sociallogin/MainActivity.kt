41ffa6e6bb0a1e393e75dbc377e0c4af2dc114b5
hildelio <hildelio@gmail.com>
2023-11-10 21:42:30 -0300

    Android - Projeto Login Social :iphone: :closed_lock_with_key: Refatorando :man_technologist:

--- app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
+++ app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.core.widget.doOnTextChanged
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputLayout
 
+const val MIN_PASSWORD_LENGTH = 4
 class MainActivity : AppCompatActivity() {
 
     private val inputEmail: TextInputLayout by lazy { findViewById(R.id.email_text_input_layout) }
@@ -17,38 +18,51 @@ class MainActivity : AppCompatActivity() {
     }
     private val loginButton: Button by lazy { findViewById(R.id.login_button) }
     private val mainLayout: ConstraintLayout by lazy { findViewById(R.id.main) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        inputEmail.editText?.doOnTextChanged { _, _, _, _ -> validateButton() }
-        inputPassword.editText?.doOnTextChanged { _, _, _, _ -> validateButton() }
+        setupTextWatchers()
 
         loginButton.setOnClickListener {
-            val isEmailValid = isEmailValid(inputEmail.editText?.text)
-            val passwordLength = inputPassword.editText?.text?.length
-            val minLength = 4
-
-            if (!isEmailValid) {
-                inputEmail.error = "Email inválido"
-            } else {
-                inputEmail.error = null
-            }
-            if (passwordLength != null) {
-                if (passwordLength <= minLength) {
-                    inputPassword.error = "Senha deve ter mais de 4 caracteres"
-                } else {
-                    inputPassword.error = null
-                }
-            }
-            successLogin()
+            handleLogin()
         }
     }
+
+    private fun setupTextWatchers() {
+        inputEmail.editText?.doOnTextChanged { _, _, _, _ -> validateButton() }
+        inputPassword.editText?.doOnTextChanged { _, _, _, _ -> validateButton() }
+    }
+
     private fun validateButton() {
         val isEmailNotEmpty = inputEmail.editText?.text?.isNotBlank() == true
         val isPasswordNotEmpty = inputPassword.editText?.text?.isNotBlank() == true
-
         loginButton.isEnabled = isEmailNotEmpty && isPasswordNotEmpty
+    }
+
+    private fun handleLogin() {
+        val isEmailValid = isEmailValid(inputEmail.editText?.text)
+        val passwordLength = inputPassword.editText?.text?.length
+
+        validateEmail(isEmailValid)
+        validatePassword(passwordLength)
+
+        if (isEmailValid && passwordLength != null && passwordLength > MIN_PASSWORD_LENGTH) {
+            successLogin()
+        }
+    }
+
+    private fun validateEmail(isEmailValid: Boolean) {
+        inputEmail.error = if (!isEmailValid) "Email inválido" else null
+    }
+
+    private fun validatePassword(passwordLength: Int?) {
+        inputPassword.error = if (passwordLength != null && passwordLength <= MIN_PASSWORD_LENGTH) {
+            "Senha deve ter mais de $MIN_PASSWORD_LENGTH caracteres"
+        } else {
+            null
+        }
     }
 
     private fun isEmailValid(email: CharSequence?): Boolean {
